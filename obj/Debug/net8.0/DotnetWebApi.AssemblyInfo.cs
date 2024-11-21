633357746476e33923737636dc9c4b7a605520d8
Gayathriveldandi <gayathri.veldandi@databeat.io>
2024-11-21 15:51:08 +0530

    Misc files added

--- obj/Debug/net8.0/DotnetWebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b98340a20988e96c2df90588b657e5f50c03febd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
